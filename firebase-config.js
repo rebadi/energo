eaab5150fa54137d7ea33a9a002d3d192f7b3a2d
970003700@qq.com <970003700@qq.com>
2023-02-28 14:29:25 +0800

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyAWx3tfS5B4ImwUijPTE3rKn_LawWbiwig",
+    authDomain: "enerogo.firebaseapp.com",
+    projectId: "enerogo",
+    storageBucket: "enerogo.appspot.com",
+    messagingSenderId: "176230231693",
+    appId: "1:176230231693:web:ed2b816e912fb081b77cc4",
+    measurementId: "G-98XM482NWS"
+};
